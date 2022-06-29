3c273442e081acc3eafca9ba41f493a3f615e060
nicosommi <nicosommi@gmail.com>
2022-06-29 14:50:36 +0200

    feat: adds failing rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/start/:path*",
+        destination:
+          "https://caisse-enregistreuse.convertflowpages.com/business-partner-sumup-pos/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
